6edd291363cbaae3ca5f84b88b9c986e84107a24
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@gmail.com>
2023-12-03 10:47:54 +0100

    Add part one day 3

---
+++ day-3/day-3.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"aoc-2023/errorHandling"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var symbolsRegex = regexp.MustCompile(`[^A-Za-z0-9\.]`)
+var digitsRegex = regexp.MustCompile(`\d+`)
+
+func parseInput(fileName string) []string {
+	data, err := os.ReadFile(fileName)
+	errorHandling.Check(err)
+
+	return strings.Split(string(data), "\n")
+}
+
+func checkSpecialChar(lines []string, digitsRange []int, lineNumber int) bool {
+	if lineNumber < 0 || lineNumber >= len(lines) {
+		return false
+	}
+
+	line := lines[lineNumber]
+
+	specialCharRanges := symbolsRegex.FindAllStringIndex(line, -1)
+
+	for _, specialCharRange := range specialCharRanges {
+		if specialCharRange[0] >= digitsRange[0]-1 && specialCharRange[0] <= digitsRange[1] {
+			return true
+		}
+	}
+
+	return false
+}
+
+func easy() {
+	lines := parseInput("input.txt")
+
+	sum := 0
+	for i, line := range lines {
+		digitsRanges := digitsRegex.FindAllStringIndex(line, -1)
+
+		for _, digitsRange := range digitsRanges {
+			prevLineMatch := checkSpecialChar(lines, digitsRange, i-1)
+			currentLineMatch := checkSpecialChar(lines, digitsRange, i)
+			nextLineMatch := checkSpecialChar(lines, digitsRange, i+1)
+
+			if prevLineMatch || currentLineMatch || nextLineMatch {
+				val, err := strconv.Atoi(line[digitsRange[0]:digitsRange[1]])
+				errorHandling.Check(err)
+
+				sum += val
+			}
+		}
+	}
+
+	fmt.Printf("The sum is %d\n", sum)
+}
+
+func main() {
+	fmt.Println("Part one")
+	easy()
+}
