9a7b85b18d17e6f7c1e955eb3e784429c5b6e5e4
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@gmail.com>
2023-12-03 11:20:02 +0100

    Add part two day 3

--- day-3/day-3.go
+++ day-3/day-3.go
@@ -10,6 +10,7 @@ import (
 )
 
 var symbolsRegex = regexp.MustCompile(`[^A-Za-z0-9\.]`)
+var gearRegex = regexp.MustCompile(`\*`)
 var digitsRegex = regexp.MustCompile(`\d+`)
 
 func parseInput(fileName string) []string {
@@ -37,6 +38,35 @@ func checkSpecialChar(lines []string, digitsRange []int, lineNumber int) bool {
 	return false
 }
 
+func findGearDigits(lines []string, specialCharIdx int, lineNumber int) [2]int {
+	var gearDigits [2]int
+	for i := lineNumber - 1; i <= lineNumber+1; i++ {
+		if i < 0 || i >= len(lines) {
+			break
+		}
+
+		line := lines[i]
+		digitsRanges := digitsRegex.FindAllStringIndex(line, -1)
+
+		for _, digitsRange := range digitsRanges {
+			if specialCharIdx >= digitsRange[0]-1 && specialCharIdx <= digitsRange[1] {
+				val, err := strconv.Atoi(line[digitsRange[0]:digitsRange[1]])
+
+				errorHandling.Check(err)
+
+				if gearDigits[0] == 0 {
+					gearDigits[0] = val
+				} else if gearDigits[1] == 0 {
+					gearDigits[1] = val
+					return gearDigits
+				}
+			}
+		}
+	}
+
+	return gearDigits
+}
+
 func easy() {
 	lines := parseInput("input.txt")
 
@@ -61,7 +91,29 @@ func easy() {
 	fmt.Printf("The sum is %d\n", sum)
 }
 
+func hard() {
+	lines := parseInput("input.txt")
+
+	sum := 0
+
+	for i, line := range lines {
+		gearIndices := gearRegex.FindAllStringIndex(line, -1)
+
+		for _, gearIdx := range gearIndices {
+			gears := findGearDigits(lines, gearIdx[0], i)
+
+			sum += gears[0] * gears[1]
+		}
+
+	}
+
+	fmt.Printf("The sum is %d\n", sum)
+}
+
 func main() {
 	fmt.Println("Part one")
 	easy()
+
+	fmt.Println("Part two")
+	hard()
 }
