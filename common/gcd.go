8c51e99a1802b8ea65a64d5bf13ced72f8fb008b
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@gmail.com>
2023-12-08 09:27:33 +0100

    Add part two day 8

---
+++ common/gcd.go
@@ -0,0 +1,26 @@
+package common
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(integers ...int) int {
+	if len(integers) < 2 {
+		panic("Not enough arguments")
+	}
+
+	result := integers[0] * integers[1] / GCD(integers[0], integers[1])
+
+	for i := 2; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
--- day-8/day-8.go
+++ day-8/day-8.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"aoc-2023/common"
 	"aoc-2023/errorHandling"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -37,43 +39,92 @@ func parseInput(fileName string) (string, map[string]([2]string)) {
 }
 
 const instructionSet = "LR"
-const startNode = "AAA"
-const endNode = "ZZZ"
 
 func getInstructionIndex(instruction byte) int {
 	return strings.IndexByte(instructionSet, instruction)
 }
 
-func walkNetwork(instructions string, connections map[string]([2]string)) int {
-	currentNode := startNode
+func getPeriods(instructions string, connections map[string]([2]string), startNodes []string, endNodes []string) ([]int, []int) {
+	currentNodes := startNodes
 
-	numSteps := 0
-	for ; currentNode != endNode; numSteps++ {
-		connection, ok := connections[currentNode]
+	leading := make([]int, len(currentNodes))
+	periods := make([]int, len(currentNodes))
 
-		if !ok {
-			panic("No connection found")
+	for i := 0; slices.Contains(leading, 0) || slices.Contains(periods, 0); i++ {
+		for nodeIdx, currentNode := range currentNodes {
+			if periods[nodeIdx] != 0 {
+				// If we already have the period, skip
+				continue
+			}
+
+			connection, ok := connections[currentNode]
+
+			if !ok {
+				panic("No connection found")
+			}
+
+			instruction := instructions[i%len(instructions)]
+			idx := getInstructionIndex(instruction)
+
+			// Travel node
+			// We can change it in place because length of array doesn't change
+			currentNodes[nodeIdx] = connection[idx]
+
+			// If we have an end node now, fill in the bookkeeping
+			if !slices.Contains(endNodes, currentNodes[nodeIdx]) {
+				continue
+			}
+
+			if leading[nodeIdx] == 0 {
+				// 0 based indexing. Actual number of steps should be + 1
+				leading[nodeIdx] = i + 1
+				continue
+			}
+
+			// Subtract the leading period
+			periods[nodeIdx] = i + 1 - leading[nodeIdx]
 		}
-
-		instruction := instructions[numSteps%len(instructions)]
-		idx := getInstructionIndex(instruction)
-
-		// Travel node
-		currentNode = connection[idx]
 	}
 
-	return numSteps
+	return leading, periods
 }
 
 func easy() {
 	instructions, connections := parseInput("input.txt")
 
-	numSteps := walkNetwork(instructions, connections)
+	_, periods := getPeriods(instructions, connections, []string{"AAA"}, []string{"ZZZ"})
+
+	numSteps := periods[0]
 
 	fmt.Printf("The number of steps is %d\n", numSteps)
+}
+
+func hard() {
+	instructions, connections := parseInput("example-input3.txt")
+
+	startNodes := make([]string, 0)
+	endNodes := make([]string, 0)
+	for key := range connections {
+		if strings.HasSuffix(key, "A") {
+			startNodes = append(startNodes, key)
+		} else if strings.HasSuffix(key, "Z") {
+			endNodes = append(endNodes, key)
+		}
+	}
+
+	leading, periods := getPeriods(instructions, connections, startNodes, endNodes)
+
+	// Printing out to manually verify that periods play nicely
+	fmt.Println(leading, periods)
+
+	commonPeriod := common.LCM(periods...)
+	fmt.Printf("The number of steps is %d\n", commonPeriod)
 }
 
 func main() {
 	fmt.Println("Part one")
 	easy()
+
+	fmt.Println("Part two")
+	hard()
 }
