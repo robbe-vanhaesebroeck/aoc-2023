ae76facc1f1fa7b4a63059dc99e88de361767a4d
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@gmail.com>
2023-12-11 09:32:13 +0100

    Add part two day 11

--- day-11/day-11.go
+++ day-11/day-11.go
@@ -75,19 +75,19 @@ func isBetween(n, lim1, lim2 int) bool {
 	return lim2 <= n && n <= lim1
 }
 
-func galaxyDistance(galaxy1, galaxy2 Coord, emptyRows, emptyColumns []int) int {
+func galaxyDistance(galaxy1, galaxy2 Coord, emptyRows, emptyColumns []int, expandFactor int) int {
 	// Compute the regular manhattan distance
 	dist := int(math.Abs(float64(galaxy1.x)-float64(galaxy2.x)) + math.Abs(float64(galaxy1.y)-float64(galaxy2.y)))
 
 	for _, row := range emptyRows {
 		if isBetween(row, galaxy1.y, galaxy2.y) {
-			dist++
+			dist += expandFactor - 1
 		}
 	}
 
 	for _, col := range emptyColumns {
 		if isBetween(col, galaxy1.x, galaxy2.x) {
-			dist++
+			dist += expandFactor - 1
 		}
 	}
 
@@ -101,10 +101,34 @@ func easy() {
 	emptyRows := findEmptyRows(spaceMap)
 	emptyColumns := findEmptyColumns(spaceMap)
 
+	expandFactor := 2
+
 	sum := 0
 	for i, g1 := range galaxies[:len(galaxies)-1] {
 		for _, g2 := range galaxies[i+1:] {
-			dist := galaxyDistance(g1, g2, emptyRows, emptyColumns)
+			dist := galaxyDistance(g1, g2, emptyRows, emptyColumns, expandFactor)
+
+			sum += dist
+		}
+	}
+
+	fmt.Printf("The sum of distances is %d\n", sum)
+}
+
+func hard() {
+	// Exactly the same as the previous one but with a much larger expand factor
+	spaceMap := parseInput("input.txt")
+
+	galaxies := findGalaxies(spaceMap)
+	emptyRows := findEmptyRows(spaceMap)
+	emptyColumns := findEmptyColumns(spaceMap)
+
+	expandFactor := 1000000
+
+	sum := 0
+	for i, g1 := range galaxies[:len(galaxies)-1] {
+		for _, g2 := range galaxies[i+1:] {
+			dist := galaxyDistance(g1, g2, emptyRows, emptyColumns, expandFactor)
 
 			sum += dist
 		}
@@ -116,4 +140,7 @@ func easy() {
 func main() {
 	fmt.Println("Part one")
 	easy()
+
+	fmt.Println("Part two")
+	hard()
 }
