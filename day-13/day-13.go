b011599802a6a817b9305a32c4c749ce820fb364
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@gmail.com>
2023-12-13 09:50:32 +0100

    Add part two day 13

--- day-13/day-13.go
+++ day-13/day-13.go
@@ -36,12 +36,23 @@ func parseInput(fileName string) []Grid {
 	return grids
 }
 
-func findRowSymmetry(grid Grid) int {
+func getDifference(r1, r2 string) int {
+	dist := 0
+	for i := range r1 {
+		if r1[i] != r2[i] {
+			dist++
+		}
+	}
+
+	return dist
+}
+
+func findRowSymmetry(grid Grid, smudges int) int {
 	// Finds the number of rows above a symmetry line
 	// Returns 0 if there is none
 
 	for i := 1; i < len(grid); i++ {
-		isSymmetric := true
+		totalSmudges := 0
 		for j := 1; i-j >= 0 || i+j-1 < len(grid); j++ {
 			// One of the rows falls out of the boundary
 			if i-j < 0 || i+j-1 >= len(grid) {
@@ -49,13 +60,14 @@ func findRowSymmetry(grid Grid) int {
 			}
 
 			// Both the rows fall in the boundary
-			if grid[i-j] != grid[i+j-1] {
-				isSymmetric = false
+			totalSmudges += getDifference(grid[i-j], grid[i+j-1])
+
+			if totalSmudges > smudges {
 				break
 			}
 		}
 
-		if isSymmetric {
+		if totalSmudges == smudges {
 			// Return the number of rows above the current one
 			return i
 		}
@@ -79,10 +91,10 @@ func transpose(grid Grid) Grid {
 	return transposed
 }
 
-func findColSymmetry(grid Grid) int {
+func findColSymmetry(grid Grid, smudges int) int {
 	// Finds the number of columns before the symmetry line
 	// Returns 0 if there is none
-	return findRowSymmetry(transpose(grid))
+	return findRowSymmetry(transpose(grid), smudges)
 }
 
 func easy() {
@@ -90,10 +102,32 @@ func easy() {
 
 	sum := 0
 	for _, grid := range grids {
-		rows := findRowSymmetry(grid)
+		var rows, cols int
 
-		cols := findColSymmetry(grid)
+		rows = findRowSymmetry(grid, 0)
 
+		if rows == 0 {
+			cols = findColSymmetry(grid, 0)
+		}
+		sum += rows*100 + cols
+	}
+
+	fmt.Printf("The sum is %d\n", sum)
+}
+
+func hard() {
+	grids := parseInput("input.txt")
+
+	sum := 0
+	for _, grid := range grids {
+
+		var rows, cols int
+
+		rows = findRowSymmetry(grid, 1)
+
+		if rows == 0 {
+			cols = findColSymmetry(grid, 1)
+		}
 		sum += rows*100 + cols
 	}
 
@@ -103,4 +137,7 @@ func easy() {
 func main() {
 	fmt.Println("Part one")
 	easy()
+
+	fmt.Println("Part two")
+	hard()
 }
