1515205e8cb6633be92eafa5b4210638a9346025
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@gmail.com>
2023-12-15 08:47:51 +0100

    Add part two day 15

--- day-15/day-15.go
+++ day-15/day-15.go
@@ -4,8 +4,18 @@ import (
 	"aoc-2023/errorHandling"
 	"fmt"
 	"os"
+	"regexp"
+	"slices"
+	"strconv"
 	"strings"
 )
+
+type Lens struct {
+	label string
+	value int
+}
+
+type Box []Lens
 
 func parseInput(fileName string) []string {
 	data, err := os.ReadFile(fileName)
@@ -31,6 +41,42 @@ func hash(s string) int {
 	return currentValue
 }
 
+var lensRegex = regexp.MustCompile(`(?P<label>\w+)-?(?:=(?P<value>\d+))?`)
+
+func hashMap(lensBoxes *[256]Box, s string) {
+	match := lensRegex.FindStringSubmatch(s)
+
+	if len(match) == 0 {
+		panic("Lens does not follow correct format")
+	}
+
+	labelIdx := lensRegex.SubexpIndex("label")
+	valueIdx := lensRegex.SubexpIndex("value")
+
+	label := match[labelIdx]
+	labelHash := hash(label)
+
+	value := match[valueIdx]
+
+	currentBox := lensBoxes[labelHash]
+
+	if value != "" {
+		parsedValue, err := strconv.Atoi(value)
+		errorHandling.Check(err)
+
+		if labelIdx := slices.IndexFunc(currentBox, func(lens Lens) bool { return lens.label == label }); labelIdx >= 0 {
+			lensBoxes[labelHash] = slices.Replace(currentBox, labelIdx, labelIdx+1, Lens{label, parsedValue})
+		} else {
+			lensBoxes[labelHash] = append(currentBox, Lens{label, parsedValue})
+		}
+
+	} else {
+		updatedBox := slices.DeleteFunc(currentBox, func(el Lens) bool { return el.label == label })
+		(*lensBoxes)[labelHash] = updatedBox
+	}
+
+}
+
 func easy() {
 	strs := parseInput("input.txt")
 
@@ -42,7 +88,28 @@ func easy() {
 	fmt.Printf("The sum of hashes is %d\n", total)
 }
 
+func hard() {
+	instructions := parseInput("input.txt")
+
+	var boxes [256]Box
+	for _, instruction := range instructions {
+		hashMap(&boxes, instruction)
+	}
+
+	focusingPower := 0
+	for boxIdx, box := range boxes {
+		for slot, lens := range box {
+			focusingPower += (1 + boxIdx) * (slot + 1) * lens.value
+		}
+	}
+
+	fmt.Printf("The total focusing power is %d\n", focusingPower)
+}
+
 func main() {
 	fmt.Println("Part one")
 	easy()
+
+	fmt.Println("Part two")
+	hard()
 }
